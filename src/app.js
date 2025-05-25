46aba18e69059afd170207b83d8516b065cf15c1
abhirajkaranmishra <abhirajkaran296@gmail.com>
2025-05-25 22:28:49 +0530

    Database connected with Application

--- src/app.js
+++ src/app.js
@@ -1,9 +1,37 @@
 const express = require("express");
 const app = express();
+ const connectDb = require("./config/database.js")
+ const User = require("./models/user.js");
 
-app.use("/user",(req,res)=>{
-  res.send("Hello World");
+app.post("/signUp", async (req,res)=>{
+  const userObj ={
+    "firstName":"Akshay",
+    "lastName":"Saini",
+    "emailId":"karan12342gmail.com",
+    "password":"1234Aks",
+    "age":"22",
+    "gender":"Male"
+   }
+   const user = new User(userObj);
+   try{
+    await user.save();
+    res.send("User Saved")
+   }
+   catch(err){
+    res.status(500).send("User not saved");
+   }
+
 })
-app.listen(3000,()=>{
-  console.log("Hello World");
+
+connectDb()
+.then(()=>{
+  console.log("Database Connected");
+  app.listen(3000,()=>{
+    console.log("printed");
+  })
 })
+.catch((err)=>{
+  console.error("Database not Connected");
+}) 
+
+
---
+++ src/config/database.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const connectDb = async ()=>{
+  await mongoose.connect("mongodb+srv://abhirajkaran484:LaMv6d8Bt8GWfDI1@developertinder.klfkxhm.mongodb.net/devTinder");
+}
+
+
+module.exports = connectDb;
